ecdbc420d127c0c16970e98dafd248099065c456
Rachel <racheljhumes@gmail.com>
2022-11-06 15:32:01 -0700

    commit tree 1ed3471b053ea74a48c0990ae9bc1f137a362204

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  /* - function isPalindrome will recieve one arguement (a string).
+     - The function should return true if the string is a palindrome
+     - The function should return false if the string is not a palindrome 
+   */
+  const first = word.charAt(0);
+  const last = word.charAt(word.length - 1);
+
+  if (first === last) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+isPalindrome("abba");
 
 /* 
   Add your pseudocode here
+  1. Create function isPalindrome with 'word' passed in as the arguement
+  2. Create variables that target the first and last letter of a word
+  3. Use those variables to set up a conditional that will 
+  3. Call the function with words from the test passed in 
 */
 
 /*
   Add written explanation of your solution here
+
 */
 
 // You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+// if (require.main === module) {
+//   // add your own custom tests in here
+console.log("Expecting: true");
+console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
+console.log("Expecting: false");
+console.log("=>", isPalindrome("robot"));
 
 module.exports = isPalindrome;
